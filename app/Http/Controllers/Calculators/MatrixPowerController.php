559d73344e69e583d88b69782358746e88bde50d
Ubaid Malik <ubaidrahim@gmail.com>
2025-10-29 18:39:44 +0500

    matrix power work start

---
+++ app/Http/Controllers/Calculators/MatrixPowerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Calculators;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MatrixPowerController extends Controller
+{
+    //
+}
