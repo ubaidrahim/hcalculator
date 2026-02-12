e27c01f2254575dcfbc827e65b261017a723e8a2
Ubaid Malik <ubaidrahim@gmail.com>
2026-02-12 11:02:10 +0500

    added cctohp to menu

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -85,6 +85,11 @@
                       <h6 class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">Hydraulic HP Calculator</h6>
                     </a>
                   </li>
+                  <li class="nav-item list-group-item border-0 p-0">
+                    <a class="dropdown-item py-2 ps-3 border-radius-md" href="{{ url('cc-to-hp-calculator') }}">
+                      <h6 class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center p-0">CC to HP Calculator</h6>
+                    </a>
+                  </li>
                 </ul>
               </div>
               <div class="row d-lg-none">
